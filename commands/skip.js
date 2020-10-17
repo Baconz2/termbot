4ddfbdf18319f5b0a04a10601a12b065284013d2
Baconz <evynnored@gmail.com>
2020-10-16 22:38:50 -0700

    fuck

--- commands/skip.js
+++
@@ -1,9 +0,0 @@
-const soundPlayer = require("../utils/soundplayer.js");
-
-exports.run = async (message) => {
-  await soundPlayer.skip(message);
-};
-
-exports.category = 7;
-exports.help = "Skips the current song";
-exports.requires = "sound";
