d72537f26b7e5e4bc5f692dc48faf81aacb51701
1e1001 <michael@malinov.com>
2021-01-11 15:40:22 -0800

    make the yes

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const Discord = require("discord.js"); // imports the discord.js library
-const fs = require("fs"); // imports the filesystem library
-const spawnSync = require("child_process").spawnSync;
-require("dotenv").config(); // load and configure .env file
+import { Client } from "discord.js"
+import { writeFile, unlink } from "fs/promises"
+import { exec } from "child_process"
+import dotenv from "dotenv"
 
-const client = new Discord.Client(); // makes new client
+dotenv.config(); // load and configure .env file
+const client = new Client(); // makes new client
+const messageStart = `${process.env.PREFIX}`;
 
 client.once("ready", () => {
-	console.log("supposedly running?");
+	console.log("bot running");
 });
 
 client.on("message", async (message) => {
-	if (message.content.startsWith(".term ")) {
-		const args = message.content.split(" ").slice(1);
-		try {
-			const comm = spawnSync(args[0], args.slice(1));
-			const evaled = `stderr: ${comm.stderr.toString()}\nstdout"${comm.stdout.toString()}`
-			const cleaned = clean(evaled);
+	if (message.content.startsWith(messageStart)) {
+		const command = message.content.slice(messageStart.length);
+		console.log("running command", command);
+		exec(command, async (_, stdout, stderr) => {
+			const formatted = `stderr:\n${stderr}\nstdout:\n${stdout}`
+			const cleaned = clean(formatted);
 			const sendString = `\`\`\`bash\n${cleaned}\n\`\`\``;
-			if (sendString.length >= 2000) {
-				return {
-					text: "The resulting output was too large, so here it is as a text file:",
-					file: cleaned,
-					name: "output.txt"
-				};
+			if (sendString.length > 2000) {
+				await writeFile(`${process.cwd()}/output.txt`, cleaned);
+				await message.channel.send("The resulting output was too large, so here it is as a text file:", {
+					files: [{
+						attachment: `${process.cwd()}/output.txt`,
+						name: "output.txt"
+					}]
+				});
+				await unlink(`${process.cwd()}/output.txt`);
 			} else {
-				return sendString;
+				await message.channel.send(sendString);
 			}
-		} catch (err) {
-			return `\`ERROR\` \`\`\`xl\n$s{clean(err)}\n\`\`\``;
-		}
+		});
 	}
 });
 
@@ -40,22 +43,13 @@ function optionalReplace(token) {
 // clean(text) to clean message of any private info or mentions
 // modified to assume text is always a string
 function clean(text) {
+  if (typeof text !== "string")
+    text = util.inspect(text, { depth: 1 });
 	return text
 		.replace(/`/g, `\`${String.fromCharCode(8203)}`)
 		.replace(/@/g, `@${String.fromCharCode(8203)}`)
 		.replace(/</g, `<${String.fromCharCode(8203)}`)
 		.replace(process.env.TOKEN, optionalReplace(process.env.TOKEN))
-		// .replace(process.env.MASHAPE, optionalReplace(process.env.MASHAPE))
-		// .replace(process.env.CAT, optionalReplace(process.env.CAT))
-		// .replace(process.env.GOOGLE, optionalReplace(process.env.GOOGLE))
-		// .replace(process.env.DBL, optionalReplace(process.env.DBL))
-		// .replace(process.env.MONGO, optionalReplace(process.env.MONGO))
-		// .replace(process.env.TWITTER_KEY, optionalReplace(process.env.TWITTER_KEY))
-		// .replace(process.env.CONSUMER_SECRET, optionalReplace(process.env.CONSUMER_SECRET))
-		// .replace(process.env.ACCESS_TOKEN, optionalReplace(process.env.ACCESS_TOKEN))
-		// .replace(process.env.ACCESS_SECRET, optionalReplace(process.env.ACCESS_SECRET));
-		
-		// these last ones are commented out because this bot doesnt have them in the .env
 };
 
 client.login(process.env.TOKEN); // starts bot with token from .env
