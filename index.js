b04f4aa7f54f58e6afeae2d611242c95af3f533f
evyn <baconz@disroot.org>
2021-01-11 08:43:35 +0000

    rewrite that may or may not work

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js"); // imports the discord.js library
+const fs = require("fs"); // imports the filesystem library
+
+const client = new Discord.Client(); // makes new client
+const token = fs.readFileSync("token.txt").toString(); // reads token from file token.txt
+
+client.once("ready", () => {
+	console.log("supposedly running?");
+
+client.on("message", message => {
+	if (message.content === ".term") {
+	const code = args.join(" ");
+		try {
+			const { spawnSync } = require( 'child_process' );
+			const comm = spawnSync( args[0], args.slice(1) );
+			const evaled = `stderr: ${comm.stderr.toString()}\nstdout"${comm.stdout.toString()}`
+			const cleaned = await clean(evaled);
+			const sendString = `\`\`\`bash\n${cleaned}\n\`\`\``;
+			if sendString.length >= 2000
+				return {
+				text: "The resulting output was too large, so here it is as a text file:",
+				file: cleaned,
+				name: "output.txt"
+			};
+		} else {
+			return sendString;
+		}
+	} catch (err) {
+		return `\`ERROR\` \`\`\`xl\n$s{await clean(err)}\n\`\`\``;
+	}
+});
+});
+
+client.login(token); // starts bot with specified token
